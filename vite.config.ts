1ed9126040663d43ba46cb2a4eb80292bc77ff2e
jaithakur098 <phe.edu08@gmail.com>
2026-06-26 00:20:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/Verify-coach/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
